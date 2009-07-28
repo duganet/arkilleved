3007071fb9aab59e94538693f2653491ac9bd350
Sergey Babneev <plughead@mail.ru>
2009-07-28 20:55:17 +0400

    перенёс исходники GLFT_Font в extern/GLFT_Font
    Signed-off-by: PlugHead <PlugHead@mail.ru>

--- src/Brick.h
+++ src/Brick.h
@@ -1,19 +1,19 @@
 /*
  *      Brick.h
- *      
+ *
  *      Copyright 2009 PlugHead Games <plughead@mail.ru>
  *      Copyright 2009 DUGA Project <dev@duganet.ru>
- *      
+ *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
  *      the Free Software Foundation; either version 2 of the License, or
  *      (at your option) any later version.
- *      
+ *
  *      This program is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *      GNU General Public License for more details.
- *      
+ *
  *      You should have received a copy of the GNU General Public License
  *      along with this program; if not, write to the Free Software
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -25,7 +25,7 @@
 #include "SDLUtils.h"
 #include "Texture.h"
 #include <vector>
-#include "GLFT_Font.h"
+#include "../extern/GLFT_Font/GLFT_Font.h"
 
 enum BrickType
 {
