ba484e51a0de770f2fb0922dd4c493aef7ad284b
Sergey Babneev <plughead@mail.ru>
2009-07-28 06:44:49 +0400

    color key in Texture class
    Signed-off-by: PlugHead <PlugHead@mail.ru>

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -56,19 +56,130 @@ bool Texture::load_from_file(std::string filename)
     }
 
     SDL_Surface *temp = SDL_CreateRGBSurface(SDL_SWSURFACE, surface->w+width, surface->h+height, surface->format->BitsPerPixel,
-                                                surface->format->Rmask,surface->format->Gmask,surface->format->Bmask,surface->format->Amask);
+                                             surface->format->Rmask,surface->format->Gmask,surface->format->Bmask,surface->format->Amask);
     SDL_Rect offset;
     offset.x = 0;
     offset.y = 0;
 
     SDL_BlitSurface(surface, NULL, temp, &offset);
 
+    //glDisable( GL_BLEND );
     // Have OpenGL generate a texture object handle for us
     glGenTextures( 1, &texture );
 
     // Bind the texture object
     glBindTexture( GL_TEXTURE_2D, texture );
+    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // Set the texture's stretching properties
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 
+    // Edit the texture object's image data using the information SDL_Surface gives us
+    glTexImage2D( GL_TEXTURE_2D, 0, mode, temp->w, temp->h, 0,
+                      mode, GL_UNSIGNED_BYTE, temp->pixels );
+
+    wdt = (float)surface->w/((float)surface->w+(float)width);
+    hgt = (float)surface->h/((float)surface->h+(float)height);
+
+    SDL_FreeSurface(temp);
+    SDL_FreeSurface(surface);
+    return true;
+}
+
+bool Texture::load_from_file(std::string filename, int r, int g, int b)
+{
+    int mode;
+    int width = 0;
+    int height = 0;
+
+    SDL_Surface *surface;
+    surface = IMG_Load(filename.c_str());
+    if(surface == NULL)
+    {
+        log("ERROR: " + filename + "not load");
+        return false;
+    }
+
+    if(surface->format->BytesPerPixel == 4) // 32bit
+    {
+        mode = GL_RGBA;
+    }
+    else if(surface->format->BytesPerPixel == 3) //24bit
+    {
+
+        mode = GL_RGBA;
+    }
+    else
+    {
+        log("ERROR: " + filename + " not 32 or 24 bit image");
+        SDL_FreeSurface(surface);
+        return false;
+    }
+    w = surface->w;
+    h = surface->h;
+
+    //Check that the image's width is a power of 2
+    while ( ((surface->w+width) & (surface->w+width - 1)) != 0 )
+    {
+        width++;
+    }
+
+    // Also check if the height is a power of 2
+    while ( (surface->h+height & (surface->h+height - 1)) != 0 )
+    {
+        height++;
+    }
+
+    int rmask,gmask,bmask,amask;
+
+    #if not SDL_BYTEORDER == SDL_BIG_ENDIAN
+     rmask = 0xff000000;
+     gmask = 0x00ff0000;
+     bmask = 0x0000ff00;
+     amask = 0x000000ff;
+ #else
+     rmask = 0x000000ff;
+     gmask = 0x0000ff00;
+     bmask = 0x00ff0000;
+     amask = 0xff000000;
+ #endif
+
+    SDL_Surface *temp = SDL_CreateRGBSurface(SDL_SWSURFACE, surface->w+width, surface->h+height, 32,
+                                                rmask,gmask,bmask,amask);
+    SDL_Rect offset;
+    offset.x = 0;
+    offset.y = 0;
+
+    SDL_BlitSurface(surface, NULL, temp, &offset);
+
+    for(int y = 0; y < surface->h; y++)
+    {
+        for(int x = 0; x < temp->w; x++)
+        {
+            unsigned int pix = ((unsigned int*)temp->pixels)[y*(temp->pitch/sizeof(unsigned int)) + x];
+            #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+            SDL_Color color = {(pix & 0x00ff0000)/0x10000,(pix & 0x0000ff00)/0x100,(pix & 0x000000ff),0};
+            #else
+            SDL_Color color = {(pix & 0x000000ff),(pix & 0x0000ff00)/0x100,(pix & 0x00ff0000)/0x10000,0};
+            #endif
+            if(color.r == r && color.g == g && color.b == b)
+            {
+                ((unsigned int*)temp->pixels)[y*(temp->pitch/sizeof(unsigned int)) + x] = SDL_MapRGBA(temp->format, 255, 0, 255,0);
+            }
+            else
+            {
+                ((unsigned int*)temp->pixels)[y*(temp->pitch/sizeof(unsigned int)) + x] = SDL_MapRGBA(temp->format, color.r, color.g, color.b,255);
+            }
+        }
+    }
+
+    // Have OpenGL generate a texture object handle for us
+    glGenTextures( 1, &texture );
+
+    // Bind the texture object
+    glBindTexture( GL_TEXTURE_2D, texture );
+    glEnable( GL_BLEND );
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     // Set the texture's stretching properties
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
--- src/Texture.h
+++ src/Texture.h
@@ -12,6 +12,7 @@ class Texture
 public:
     ~Texture();
     bool load_from_file(std::string filename);
+    bool load_from_file(std::string filename, int r, int g, int b);
     bool load_from_surface(SDL_Surface* source);
     void show(int x, int y);
     GLuint get_texture(){return texture;}
